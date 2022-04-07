f5670b52a3cfa8a5712d744e6f8e94543b67d010
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-04-07 12:44:37 +0200

    Added some animations

--- app.js
+++ app.js
@@ -4,10 +4,9 @@
     const form = document.getElementById("form")
     const email = document.getElementById("enter-email").value
     const text = document.getElementById("text")
-    const button = document.querySelector(".email-btn")
     const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-button.addEventListener("click", () => {
+
 
     
  if (email.match(pattern)){
@@ -15,7 +14,7 @@ button.addEventListener("click", () => {
     form.classList.remove("invalid")
  
  
-    text.innerHTML = "You email adress is valid !"
+    text.innerHTML = "Your email adress is valid !"
     text.style.color = "#00ff00"
     
  }
@@ -39,8 +38,7 @@ button.addEventListener("click", () => {
    
  
  
- 
-})
+
 
  
 
